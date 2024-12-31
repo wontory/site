ee42d4f55909ba0da2e7064f3f674400c6c2a3d4
조성원 <tjddnjsjo@kakao.com>
2025-01-01 03:43:14 +0900

    chore: rename package

--- packages/design-system/turbo/generators/config.cjs
+++ packages/design-system/turbo/generators/config.cjs
@@ -1,10 +1,8 @@
-import type { PlopTypes } from '@turbo/gen'
-
-// Learn more about Turborepo Generators at https://turbo.build/repo/docs/core-concepts/monorepos/code-generation
-
-export default function generator(plop: PlopTypes.NodePlopAPI): void {
-  // A simple generator to add a new React component to the internal UI library
-  plop.setGenerator('react-component', {
+/**
+ * @param {import('@turbo/gen').PlopTypes.NodePlopAPI} plop
+ */
+function generator(plop) {
+  plop.setGenerator('component', {
     description: 'Adds a new react component',
     prompts: [
       {
@@ -28,3 +26,5 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
     ],
   })
 }
+
+module.exports = generator
