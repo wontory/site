aa942d551ad754cee20520751dc786721aeb2a0c
조성원 <tjddnjsjo@kakao.com>
2025-01-03 18:14:46 +0900

    chore: add font pretendard

--- apps/playground/app/layout.tsx
+++ apps/playground/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 
-import '~/styles/globals.css'
+import { pretendard } from '@package/design-system/fonts/pretendard'
+import '@package/design-system/styles/globals.css'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -13,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={pretendard.variable}>
       <body>{children}</body>
     </html>
   )
--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@package/design-system/components/button'
+
 export default function Home() {
-  return <div className="font-bold text-3xl underline">Hello www!</div>
+  return (
+    <>
+      <div className="font-bold text-3xl underline">Hello www!</div>
+      <Button>Button</Button>
+    </>
+  )
 }
