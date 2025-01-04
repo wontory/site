7e209c6c489cbd58686983e22a3f7b67d3a21206
Seongwon Jo <tjddnjsjo@kyonggi.ac.kr>
2025-01-04 15:44:20 +0900

    revert: rollback (#11)
    * fix: set base path
    
    * fix: fix wild card for routing rewrites
    
    * chore: add start script
    
    * chore: add trailing slash
    
    * chore: add hard coded domain
    
    * chore: add hard coded domain
    
    * revert: rollback

--- apps/www/next.config.ts
+++ apps/www/next.config.ts
@@ -3,34 +3,32 @@ import type { NextConfig } from 'next'
 const { BLOG_DOMAIN, PLAYGROUND_DOMAIN } = process.env
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/blog',
-        destination: `${BLOG_DOMAIN}/blog`,
-      },
-      {
-        source: '/blog/:path+',
-        destination: `${BLOG_DOMAIN}/blog/:path+`,
-      },
-      {
-        source: '/blog-static/:path+',
-        destination: `${BLOG_DOMAIN}/blog-static/:path+`,
-      },
-      {
-        source: '/playground',
-        destination: `${PLAYGROUND_DOMAIN}/playground`,
-      },
-      {
-        source: '/playground/:path+',
-        destination: `${PLAYGROUND_DOMAIN}/playground/:path+`,
-      },
-      {
-        source: '/playground-static/:path+',
-        destination: `${PLAYGROUND_DOMAIN}/playground-static/:path+`,
-      },
-    ]
-  },
+  rewrites: async () => [
+    {
+      source: '/blog',
+      destination: `${BLOG_DOMAIN}`,
+    },
+    {
+      source: '/blog/:path+',
+      destination: `${BLOG_DOMAIN}/:path+`,
+    },
+    {
+      source: '/blog-static/:path+',
+      destination: `${BLOG_DOMAIN}/blog-static/:path+`,
+    },
+    {
+      source: '/playground',
+      destination: `${PLAYGROUND_DOMAIN}`,
+    },
+    {
+      source: '/playground/:path+',
+      destination: `${PLAYGROUND_DOMAIN}/:path+`,
+    },
+    {
+      source: '/playground-static/:path+',
+      destination: `${PLAYGROUND_DOMAIN}/playground-static/:path+`,
+    },
+  ],
 }
 
 export default nextConfig
