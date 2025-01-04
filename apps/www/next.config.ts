1d922ccbbb1e5803e25ed0d427191a8c066c80d1
조성원 <tjddnjsjo@kakao.com>
2025-01-04 15:33:22 +0900

    chore: add hard coded domain

--- apps/www/next.config.ts
+++ apps/www/next.config.ts
@@ -1,36 +1,37 @@
 import type { NextConfig } from 'next'
 
-const { BLOG_DOMAIN, PLAYGROUND_DOMAIN } = process.env
+const { BLOG_DOMAIN, PLAYGROUND_DOMAIN } = process.env ?? {
+  BLOG_DOMAIN: 'https://preview.blog.wontory.dev/blog',
+  PLAYGROUND_DOMAIN: 'https://preview.playground.wontory.dev/playground',
+}
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/blog',
-        destination: `${BLOG_DOMAIN}/blog/`,
-      },
-      {
-        source: '/blog/:path+',
-        destination: `${BLOG_DOMAIN}/blog/:path+/`,
-      },
-      {
-        source: '/blog-static/:path+',
-        destination: `${BLOG_DOMAIN}/blog-static/:path+/`,
-      },
-      {
-        source: '/playground',
-        destination: `${PLAYGROUND_DOMAIN}/playground/`,
-      },
-      {
-        source: '/playground/:path+',
-        destination: `${PLAYGROUND_DOMAIN}/playground/:path+/`,
-      },
-      {
-        source: '/playground-static/:path+',
-        destination: `${PLAYGROUND_DOMAIN}/playground-static/:path+/`,
-      },
-    ]
-  },
+  rewrites: async () => [
+    {
+      source: '/blog',
+      destination: `${BLOG_DOMAIN}/blog/`,
+    },
+    {
+      source: '/blog/:path+',
+      destination: `${BLOG_DOMAIN}/blog/:path+/`,
+    },
+    {
+      source: '/blog-static/:path+',
+      destination: `${BLOG_DOMAIN}/blog-static/:path+/`,
+    },
+    {
+      source: '/playground',
+      destination: `${PLAYGROUND_DOMAIN}/playground/`,
+    },
+    {
+      source: '/playground/:path+',
+      destination: `${PLAYGROUND_DOMAIN}/playground/:path+/`,
+    },
+    {
+      source: '/playground-static/:path+',
+      destination: `${PLAYGROUND_DOMAIN}/playground-static/:path+/`,
+    },
+  ],
 }
 
 export default nextConfig
