5cf3f6b79f66f24b56676b26d99724bf244faa64
조성원 <tjddnjsjo@kakao.com>
2025-01-04 15:43:02 +0900

    revert: rollback

--- apps/playground/next.config.ts
+++ apps/playground/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  basePath: '/playground',
   assetPrefix: '/playground-static',
 }
 
--- apps/www/next.config.ts
+++ apps/www/next.config.ts
@@ -6,27 +6,27 @@ const nextConfig: NextConfig = {
   rewrites: async () => [
     {
       source: '/blog',
-      destination: `${BLOG_DOMAIN ?? 'https://preview.blog.wontory.dev'}/blog/`,
+      destination: `${BLOG_DOMAIN}`,
     },
     {
       source: '/blog/:path+',
-      destination: `${BLOG_DOMAIN ?? 'https://preview.blog.wontory.dev'}/blog/:path+/`,
+      destination: `${BLOG_DOMAIN}/:path+`,
     },
     {
       source: '/blog-static/:path+',
-      destination: `${BLOG_DOMAIN ?? 'https://preview.blog.wontory.dev'}/blog-static/:path+/`,
+      destination: `${BLOG_DOMAIN}/blog-static/:path+`,
     },
     {
       source: '/playground',
-      destination: `${PLAYGROUND_DOMAIN ?? 'https://preview.playground.wontory.dev'}/playground/`,
+      destination: `${PLAYGROUND_DOMAIN}`,
     },
     {
       source: '/playground/:path+',
-      destination: `${PLAYGROUND_DOMAIN ?? 'https://preview.playground.wontory.dev'}/playground/:path+/`,
+      destination: `${PLAYGROUND_DOMAIN}/:path+`,
     },
     {
       source: '/playground-static/:path+',
-      destination: `${PLAYGROUND_DOMAIN ?? 'https://preview.playground.wontory.dev'}/playground-static/:path+/`,
+      destination: `${PLAYGROUND_DOMAIN}/playground-static/:path+`,
     },
   ],
 }
