9b62b1fc684ca599fde426c9f358073c74180043
조성원 <tjddnjsjo@kakao.com>
2025-01-14 02:39:18 +0900

    chore(blog): configure velite

--- apps/blog/velite.config.ts
+++ apps/blog/velite.config.ts
@@ -1,95 +1,62 @@
-import { exec } from 'node:child_process'
-import { promisify } from 'node:util'
+import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections'
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
+import rehypeExpressiveCode, {
+  type RehypeExpressiveCodeOptions,
+} from 'rehype-expressive-code'
 import { defineCollection, defineConfig, s } from 'velite'
 
-const slugify = (input: string) =>
-  input
-    .toLowerCase()
-    .replace(/\s+/g, '-')
-    .replace(/[^a-z0-9-]/g, '')
+const rehypeExpressiveCodeOptions: RehypeExpressiveCodeOptions = {
+  themes: ['min-dark', 'min-light'],
+  themeCssSelector: (theme) => `.${theme.type}`,
+  plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
+}
 
-const icon = s.enum(['github', 'instagram', 'medium', 'twitter', 'youtube'])
-const count = s
-  .object({ total: s.number(), posts: s.number() })
-  .default({ total: 0, posts: 0 })
+const slugify = (slug: string) => slug.split('/').slice(1).join('/')
 
-const meta = s
-  .object({
-    title: s.string().optional(),
-    description: s.string().optional(),
-    keywords: s.array(s.string()).optional(),
-  })
-  .default({})
-
-const execAsync = promisify(exec)
-
-const timestamp = () =>
-  s
-    .custom<string | undefined>((i) => i === undefined || typeof i === 'string')
-    .transform<string>(async (value, { meta, addIssue }) => {
-      if (value != null) {
-        addIssue({
-          fatal: false,
-          code: 'custom',
-          message:
-            '`s.timestamp()` schema will resolve the value from `git log -1 --format=%cd`',
-        })
-      }
-      const { stdout } = await execAsync(`git log -1 --format=%cd ${meta.path}`)
-      return new Date(stdout || Date.now()).toISOString()
-    })
-
-const categories = defineCollection({
-  name: 'Category',
-  pattern: 'categories/*.yml',
-  schema: s
-    .object({
-      name: s.string().max(20),
-      slug: s.slug('global', ['admin', 'login']),
-      cover: s.image().optional(),
-      description: s.string().max(999).optional(),
-      count,
-    })
-    .transform((data) => ({ ...data, permalink: `/${data.slug}` })),
+const calculateReadingTime = (wordCount: number) => ({
+  readingTime: Math.round(wordCount / 120),
 })
 
-const tags = defineCollection({
-  name: 'Tag',
-  pattern: 'tags/index.yml',
+const memo = defineCollection({
+  name: 'Memo',
+  pattern: 'memo/**/*.mdx',
   schema: s
     .object({
-      name: s.string().max(20),
-      slug: s.slug('global', ['admin', 'login']),
-      cover: s.image().optional(),
-      description: s.string().max(999).optional(),
-      count,
-    })
-    .transform((data) => ({ ...data, permalink: `/${data.slug}` })),
-})
-
-const posts = defineCollection({
-  name: 'Post',
-  pattern: 'posts/**/*.md',
-  schema: s
-    .object({
-      title: s.string().max(99),
-      slug: s.slug('post'),
-      date: s.isodate(),
-      updated: timestamp(),
-      cover: s.image().optional(),
-      video: s.file().optional(),
-      description: s.string().max(999).optional(),
-      draft: s.boolean().default(false),
-      featured: s.boolean().default(false),
-      categories: s.array(s.string()).default(['Journal']),
-      tags: s.array(s.string()).default([]),
-      meta: meta,
-      toc: s.toc(),
       metadata: s.metadata(),
-      excerpt: s.excerpt(),
-      content: s.markdown(),
+      slug: s.path(),
+      title: s.string().max(99),
+      description: s.string().max(999).optional(),
+      date: s.isodate(),
+      draft: s.boolean().default(false),
+      content: s.mdx(),
+      toc: s.toc(),
     })
-    .transform((data) => ({ ...data, permalink: `/blog/${data.slug}` })),
+    .transform((data) => ({
+      ...data,
+      metadata: calculateReadingTime(data.metadata.wordCount),
+      slug: slugify(data.slug),
+    })),
+})
+
+const post = defineCollection({
+  name: 'Post',
+  pattern: 'post/**/*.mdx',
+  schema: s
+    .object({
+      metadata: s.metadata(),
+      slug: s.path(),
+      title: s.string().max(99),
+      description: s.string().max(999).optional(),
+      date: s.isodate(),
+      draft: s.boolean().default(false),
+      content: s.mdx(),
+      toc: s.toc(),
+    })
+    .transform((data) => ({
+      ...data,
+      metadata: calculateReadingTime(data.metadata.wordCount),
+      slug: slugify(data.slug),
+    })),
 })
 
 export default defineConfig({
@@ -101,45 +68,9 @@ export default defineConfig({
     name: '[name]-[hash:6].[ext]',
     clean: true,
   },
-  collections: { categories, tags, posts },
-  prepare: ({ categories, tags, posts }) => {
-    const docs = posts.filter(
-      (i) => process.env.NODE_ENV !== 'production' || !i.draft,
-    )
-    const categoriesFromDoc = Array.from(
-      new Set(docs.flatMap((i) => i.categories)),
-    ).filter((i) => categories.find((j) => j.name === i) == null)
-    categories.push(
-      ...categoriesFromDoc.map((name) => ({
-        name,
-        slug: slugify(name),
-        permalink: '',
-        count: { total: 0, posts: 0 },
-      })),
-    )
-    for (const category of categories) {
-      category.count.posts = posts.filter((j) =>
-        j.categories.includes(category.name),
-      ).length
-      category.count.total = category.count.posts
-      category.permalink = `/${category.slug}`
-    }
-
-    const tagsFromDoc = Array.from(new Set(docs.flatMap((i) => i.tags))).filter(
-      (i) => tags.find((j) => j.name === i) == null,
-    )
-    tags.push(
-      ...tagsFromDoc.map((name) => ({
-        name,
-        slug: slugify(name),
-        permalink: '',
-        count: { total: 0, posts: 0 },
-      })),
-    )
-    for (const tag of tags) {
-      tag.count.posts = posts.filter((j) => j.tags.includes(tag.name)).length
-      tag.count.total = tag.count.posts
-      tag.permalink = `/${tag.slug}`
-    }
+  collections: { memo, post },
+  mdx: {
+    rehypePlugins: [[rehypeExpressiveCode, rehypeExpressiveCodeOptions]],
+    remarkPlugins: [],
   },
 })
