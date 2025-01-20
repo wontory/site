dda34a16ba4f2e98148df86e5084dcf1c823c25d
조성원 <tjddnjsjo@kakao.com>
2025-01-21 00:54:44 +0900

    feat(blog): create post-card component

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -1,10 +1,20 @@
-import { Button } from '@package/ui/components/button'
+import Link from 'next/link'
+
+import { PostCard } from '#components/post-card'
+import { post } from '#site/content'
 
 export default function Home() {
+  const contents = post.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
+  )
+
   return (
-    <>
-      <div className="font-bold text-3xl underline">Hello blog!</div>
-      <Button>Button</Button>
-    </>
+    <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3">
+      {contents.map((content) => (
+        <Link key={content.slug} href={`/blog/${content.slug}`}>
+          <PostCard content={content} />
+        </Link>
+      ))}
+    </div>
   )
 }
---
+++ apps/blog/src/components/post-card.tsx
@@ -0,0 +1,39 @@
+import { format } from 'date-fns'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@package/ui/components/card'
+import { ClockIcon } from '@package/ui/icons/lucide'
+
+import type { Post } from '#site/content'
+
+function PostCard({ content }: { content: Post }) {
+  return (
+    <Card className="transition-colors hover:bg-primary/5">
+      <CardHeader className="p-4">
+        <span className="text-primary/60 text-sm">
+          {format(content.date, 'PPP')}
+        </span>
+      </CardHeader>
+      <CardContent className="space-y-1 px-4 pt-0 pb-4">
+        <CardTitle className="text-lg">{content.title}</CardTitle>
+        <CardDescription className="line-clamp-2">
+          {content.description}
+        </CardDescription>
+      </CardContent>
+      <CardFooter className="px-4 pt-0 pb-4">
+        <span className="flex items-center gap-2 text-xs">
+          <ClockIcon className="size-3" />
+          {content.metadata.readingTime} min read
+        </span>
+      </CardFooter>
+    </Card>
+  )
+}
+
+export { PostCard }
