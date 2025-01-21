0daccc6330b42779a1371b4afe0bdb17b69259bf
조성원 <tjddnjsjo@kakao.com>
2025-01-21 17:33:09 +0900

    refactor(blog): replace grid view to virtualized masonry layout

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -1,6 +1,4 @@
-import Link from 'next/link'
-
-import { PostCard } from '#components/post-card'
+import { ContentList } from '#components/content-list'
 import { post } from '#site/content'
 
 export default function Home() {
@@ -8,13 +6,5 @@ export default function Home() {
     (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
   )
 
-  return (
-    <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3">
-      {contents.map((content) => (
-        <Link key={content.slug} href={`/blog/${content.slug}`}>
-          <PostCard content={content} />
-        </Link>
-      ))}
-    </div>
-  )
+  return <ContentList contents={contents} />
 }
