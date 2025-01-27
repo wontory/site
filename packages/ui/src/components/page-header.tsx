8742433c7a5647119484463e320886ee5fecef3b
Seongwon Jo <tjddnjsjo@kyonggi.ac.kr>
2025-01-28 04:10:17 +0900

    chore: sync branches (#47)
    * docs(blog): memo blog migration (#44)
    
    * refactor(blog): optimize sort logic
    
    * docs(blog): memo blog migration
    
    * chore: remove memo demo
    
    * feat: add dark mode (#46)
    
    * refactor(blog): optimize sort logic
    
    * docs(blog): memo blog migration
    
    * chore: remove memo demo
    
    * fix(blog): fix image size
    
    * feat: add dark mode
    
    * refactor(ui): add children prop on page-header component
    
    * refactor(blog): refactor metadata
    
    * chore(ui): install `@radix-ui/react-dropdown-menu`
    
    * fix(ui): override dark variant to use class selector
    
    * feat(ui): create dropdown-menu component
    
    * feat(ui): add mode-toggle component into base layout
    
    * feat(ui): add dark mode transition
    
    * feat(blog): add content metadata into page-header of content view page
    
    * feat(ui): replace mode-toggle component's dropdown-menu with button
    
    * chore(ui): remove dropdown-menu component

--- packages/ui/src/components/page-header.tsx
+++ packages/ui/src/components/page-header.tsx
@@ -1,13 +1,17 @@
-function PageHeader({
-  title,
-  description,
-}: { title: string; description: string }) {
+export interface PageHeaderProps {
+  children?: React.ReactNode
+  title: string
+  description: string
+}
+
+function PageHeader({ children, title, description }: PageHeaderProps) {
   return (
     <div className="flex flex-col items-center justify-center gap-2 rounded-2xl bg-primary p-16">
       <h1 className="font-semibold text-3xl text-primary-foreground">
         {title}
       </h1>
       <span className="text-primary-foreground/60 text-sm">{description}</span>
+      {children}
     </div>
   )
 }
